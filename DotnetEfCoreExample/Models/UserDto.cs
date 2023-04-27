10045748ac0ef30234796d051e3347e60db5d8ed
BeyzaNurPelv <beyzanurpelvanoglu@gmail.com>
2023-04-28 01:26:15 +0300

    Add project files.

---
+++ DotnetEfCoreExample/Models/UserDto.cs
@@ -0,0 +1,9 @@
+﻿namespace DotnetEfCoreExample.Models
+{
+    public class UserDto
+    {
+        public required string UserName { get; set; }
+        public required string PasswordHash { get; set; }=string.Empty;
+        public string Password { get; internal set; }
+    }
+}
---
+++ DotnetEfCoreExample/Program.cs
@@ -0,0 +1,54 @@
+using DotnetEfCoreExample.Data;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options=>
+{
+    options.AddSecurityDefinition("oauth2", new OpenApiSecuritySchema
+    {
+        In=ParameterLocation.Header,
+        Name="Authorization",
+        Type=SecuritySchemeType.ApiKey
+
+    });
+    options.OperationFilter<SecurityRequirementsOperationFilter>();
+});
+builder.Services.AddAuthentication().AddJwtBearer(options=>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        ValidateAudience = false,
+        ValidateIssuer = false,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration.GetSection("AppSettings:Token").Value!))
+    };
+});
+builder.Services.AddDbContext<DataContext>();
+
+var app = builder.Build();
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseAuthentication();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
